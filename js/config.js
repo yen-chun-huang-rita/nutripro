395d9211ba89cb84046ab2ef39908cb6410511e5
Rita <ritayenchun@gmail.com>
2026-04-21 22:20:42 +0800

    Add USE_CLOUD configuration option

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   GAS_URL: 'https://script.google.com/macros/s/AKfycbyHMdOMbFY-6BzHZqN-UToSBCUd7Ex1OZKlQ1KHiJndX_hVNYKeXtXLmpYgBD4F4cHQ3Q/exec',
+  USE_CLOUD: true,
   TARGET: { kcal:1380, protein:97, carb:151, fat:43, water:2000 },
   BODY:   { height:0, age:0 },
   GOAL:   { weight:0 },
